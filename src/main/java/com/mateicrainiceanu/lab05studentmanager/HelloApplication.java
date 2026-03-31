e3d2366d6d93b895a88b857212e0fa15a4866a24
mateicrainiceanu <matei.crainiceanu@gmail.com>
2026-03-31 18:41:21 +0300

    added LoginScreen

--- src/main/java/com/mateicrainiceanu/lab05studentmanager/HelloApplication.java
+++ src/main/java/com/mateicrainiceanu/lab05studentmanager/HelloApplication.java
@@ -2,18 +2,20 @@ package com.mateicrainiceanu.lab05studentmanager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root =
+                FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/login.fxml")));
+        primaryStage.setTitle("FIS - Student Manager");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
     }
 }
